0b3bc1e9f7da3b7376cc9088b0eed85264de9786
yashxmittal <mittal.yash3333@gmail.com>
2023-07-04 15:43:30 +0530

    a

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
+const reader = require('xlsx')
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello() {
+    return await this.readExcel()
+
+  }
+
+  async readExcel() {
+    const file = reader.readFile('src/coupon.xlsx')
+    let data = []
+
+    const sheets = file.SheetNames
+
+    for (let i = 0; i < sheets.length; i++) {
+      const temp = reader.utils.sheet_to_json(
+        file.Sheets[file.SheetNames[i]])
+      temp.forEach((res) => {
+        data.push(res)
+      })
+    }
+    let result = []
+    data.forEach((item) => {
+      result.push(item['Random Coupon Code'])
+    })
+    return result
   }
 }
